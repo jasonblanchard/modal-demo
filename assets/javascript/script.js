131ec0f4888d477936df63a0243ccb061cdcbfdf
Jason Blanchard <jas.blanchard@gmail.com>
2016-01-13 19:39:27 -0500

    working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+$(document).ready(function() {
+
+  $('.reveal').on('click', function() {
+    var modal = "<div class='modal-wrapper'><div class='modal'><button class='close-modal'>x</button><div class='modal-content'>Sed posuere consectetur est at lobortis. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</div></div></div>";
+    $('body').append(modal);
+  });
+});
+
+$(document).on('click', '.close-modal', function() {
+  $('.modal-wrapper').remove();
+});
